0aaf4076d46d8e6150e5c4292d841ffab885494e
Gabriel Soares <gsoaresdesouza@live.com>
2024-07-01 08:46:17 -0300

    Começando a inteligência da Página Principal

--- modal.js
+++ modal.js
@@ -1,7 +1,4 @@
-const overlay = document.getElementById("modal-overlay");
 const background = document.getElementById("modal-background");
-
-overlay.classList.add("open");
 
 function backgroundClickHandler() {
 overlay.classList.remove("open");
