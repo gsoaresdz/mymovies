8eedbfd8f46efd36bfd926a8c4abf8dda2061d48
Gabriel Soares <gsoaresdesouza@live.com>
2024-07-01 10:57:17 -0300

    Consumindo a API do OMDB

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const overlay = document.getElementById("modal-overlay");
 const movieName = document.getElementById("movie-name");
 const movieYear = document.getElementById("movie-year");
 
-function searchButtonClickHandler() {
+async function searchButtonClickHandler() {
+let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName.value
+.split(" ")
+.join("+")}&${movieYear.value}`;
+
+const response = await fetch(url);
+const data = await response.json();
+console.log("data: ", data);
 overlay.classList.add("open");
-console.log(movieName.value.split(' ').join('+'));
-console.log("Ano: ", movieYear.value)
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
