391b5c5b15628282fbc537b5580091feed20fad9
Gabriel Soares <gsoaresdesouza@live.com>
2024-07-01 10:37:09 -0300

    Capturando as entradas do usuário

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const searchButton = document.getElementById("search-button");
-
 const overlay = document.getElementById("modal-overlay");
+const movieName = document.getElementById("movie-name");
+const movieYear = document.getElementById("movie-year");
+
 function searchButtonClickHandler() {
 overlay.classList.add("open");
+console.log(movieName.value.split(' ').join('+'));
+console.log("Ano: ", movieYear.value)
 }
 
 searchButton.addEventListener("click", searchButtonClickHandler);
