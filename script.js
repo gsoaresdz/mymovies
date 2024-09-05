f7c0bf290fecd4b1276881b8ad407a304a51b6f8
Gabriel Soares <gsoaresdesouza@live.com>
2024-09-05 12:21:02 -0300

    Trata erros, Adiciona notificação ao usuário e Adiciona modal dinâmico

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const searchButton = document.getElementById("search-button");
-const overlay = document.getElementById("modal-overlay");
-const movieName = document.getElementById("movie-name");
-const movieYear = document.getElementById("movie-year");
-
+const searchButton = document.getElementById('search-button');
+const overlay = document.getElementById('modal-overlay');
+const movieName = document.getElementById('movie-name');
+const movieYear = document.getElementById('movie-year');
 async function searchButtonClickHandler() {
-let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieName.value
-.split(" ")
-.join("+")}&${movieYear.value}`;
-
-const response = await fetch(url);
-const data = await response.json();
-console.log("data: ", data);
-overlay.classList.add("open");
+  try {
+    let url = `http://www.omdbapi.com/?
+apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('data: ', data);
+    if (data.Error) {
+      throw new Error('Filme não encontrado');
+    }
+    createModal(data);
+    overlay.classList.add('open');
+  } catch (error) {
+    notie.alert({ type: 'error', text: error.message });
+  }
 }
-
-searchButton.addEventListener("click", searchButtonClickHandler);
+function movieNameParameterGenerator() {
+  if (movieName.value === '') {
+    throw new Error('O nome do filme deve ser informado');
+  }
+  return movieName.value.split(' ').join('+');
+}
+function movieYearParameterGenerator() {
+  if (movieYear.value === '') {
+    return '';
+  }
+  if (movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
+    throw new Error('Ano do filme inválido.');
+  }
+  return `&y=${movieYear.value}`;
+}
+searchButton.addEventListener('click', searchButtonClickHandler);
